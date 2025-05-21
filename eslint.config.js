89fc8ce88f82c8542a5a588df8b62c154b281a37
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-21 11:22:44 +0100

    fix: dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,33 @@
-// eslint.config.js
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const compat = new FlatCompat();
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+  // Use the recommended ESLint config
+  ...compat.extends('eslint:recommended'),
+
+  // Use TypeScript parser and plugin
   {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
     rules: {
       'prettier/prettier': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
+
+  // Prettier recommended config
+  ...compat.extends('plugin:prettier/recommended'),
 ];
