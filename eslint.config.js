c46c72ccc6c8444a50686e3e0ddf1ceb1dd8f931
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-21 12:03:43 +0100

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,33 @@
 // eslint.config.js
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import parser from "@typescript-eslint/parser";
+import plugin from "@typescript-eslint/eslint-plugin";
 
 const compat = new FlatCompat();
 
 export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...compat.config({
-    extends: [
-      "plugin:@typescript-eslint/recommended",
-    ],
-    plugins: [],
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json", // optional if not using type-aware rules
+      },
+    },
+    plugins: {
+      "@typescript-eslint": plugin,
+    },
     rules: {
+      ...plugin.configs.recommended.rules,
       "@typescript-eslint/explicit-function-return-type": "warn",
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_" },
       ],
     },
-  }),
+  },
 ];
