b0acacade3538bf76a93a3f549af67a73ac765ce
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-21 12:13:12 +0100

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 // eslint.config.js
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
+// import js from "@eslint/js";
 import parser from "@typescript-eslint/parser";
 import plugin from "@typescript-eslint/eslint-plugin";
 
-// const compat = new FlatCompat();
 
 export default [
   js.configs.recommended,
